941834879f4377b90016697bc70bee9381a36d57
Christian <ch.grimm@gmx.net>
2024-09-25 21:45:05 +0200

    colorschemes auskommentiert.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -60,7 +60,7 @@ return {
           dark_background = true,
         },
       }
-      -- require('nordic').load()
+      require('nordic').load()
     end,
   },
   {
@@ -112,11 +112,28 @@ return {
       }
     end,
   },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    lazy = false,
-    priority = 1000,
-    config = function() end,
-  },
+  ---  {
+  ---    'rose-pine/neovim',
+  ---    name = 'rose-pine',
+  ---    lazy = false,
+  ---    priority = 1000,
+  ---    config = function() end,
+  ---  },
+  ---  {
+  ---    'echasnovski/mini.hues',
+  ---    lazy = false,
+  ---    priority = 1000,
+  ---    config = function()
+  ---      local setup = require('mini.hues').setup
+  ---      ---   -- Choose background and foreground
+  ---      ---   setup({ background = '#2f1c22', foreground = '#cdc4c6' }) -- red
+  ---      ---   setup({ background = '#2f1e16', foreground = '#cdc5c1' }) -- orange
+  ---      ---   setup({ background = '#282211', foreground = '#c9c6c0' }) -- yellow
+  ---      ---   setup({ background = '#1c2617', foreground = '#c4c8c2' }) -- green
+  ---      ---   setup({ background = '#112723', foreground = '#c0c9c7' }) -- cyan
+  ---      ---   setup({ background = '#11262d', foreground = '#c0c8cc' }) -- azure
+  ---      setup { background = '#1d2231', foreground = '#c4c6cd' } -- blue
+  ---      ---   setup({ background = '#281e2c', foreground = '#c9c5cb' }) -- purple
+  ---    end,
+  ---  },
 }
